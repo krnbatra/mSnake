bde5ea468bddd9c486729bf320285a77448f432b
Abhishek Gupta <abhishekgupt34@gmail.com>
2017-04-24 17:17:23 +0530

    Faulty TCP

--- server.c
+++ server.c
@@ -30,52 +30,60 @@ player_connection_data conn_info[MAX_PLAYERS];
 int alive[MAX_PLAYERS];
 char network_data[MAX_PLAYERS];
 int serverSocket;
+int socket_data[MAX_PLAYERS];
 
 
 void stop_listening(int signal){
     wait_min = 0;
 }
 
-void receive_moves(){
-    int arr[2] = {};
-    wait_min = 1;
-    start_timer(0, 1000000/FPS);
-    while (wait_min){
-        if(recvfrom(serverSocket, arr, 2*sizeof(int), 0, NULL, 0) == -1);
-        else {
-            printf("Received move from %d = %d/%c\n", arr[0], arr[1], arr[1]);
-            network_data[arr[0]] = arr[1];
-        }
-    }
-}
-
-void send_moves(){
-    struct sockaddr_in client_addr;
-    client_addr.sin_family = AF_INET;
-    for(int i = 0;i < num_of_connected_players; i++){
-        if (alive[i] == 0) continue;
-        client_addr.sin_addr.s_addr = inet_addr(conn_info[i].ipaddr);
-        client_addr.sin_port = htons(conn_info[i].port_no);
-        if(sendto(serverSocket, network_data,num_of_connected_players*sizeof(char), 0, (struct sockaddr*)&client_addr, sizeof(client_addr)) != num_of_connected_players*sizeof(char))
-            perror("ERROR IN SENDING\n");
-    }
-}
-
-void* work_pthread(void * dataptr){
-    int *arr = (int*)dataptr;
-    int newSocket = arr[0];
-    int player_id = arr[1];
-    size_t nr = recv(newSocket, &conn_info[player_id], sizeof(player_connection_data), 0);
-    if (nr != sizeof(player_connection_data)) { printf("Data not received properly\n");  exit(2); }
-    printf("Client connected --> ip = %s | port_no = %d | name = %s\n", 
-                conn_info[player_id].ipaddr, conn_info[player_id].port_no, conn_info[player_id].name);
+void* work_sender(void *dataptr){
+    int i;
     while (wait_min) {
         sleep(1);
     }
-    size_t ns = send(newSocket, &num_of_connected_players, sizeof(int), 0);
-    ns = send(newSocket, conn_info, num_of_connected_players*sizeof(player_connection_data), 0);
-    if (ns != sizeof(player_connection_data)*num_of_connected_players){
-        perror("Failed to send player_information");
+    sleep(1);
+    while (1){
+        sleep(1.0/FPS);
+        for (i=0;i<num_of_connected_players;i++){
+            if (!alive[i]) continue;
+            send(socket_data[i], network_data, num_of_connected_players*sizeof(char), 0);
+        }
+        if (num_of_alive_players==0) break;
+    }
+    pthread_exit(NULL);
+}
+
+void* client_handler(void * dataptr){
+    int *arr = (int*)dataptr;
+    int newSocket = arr[0];
+    int player_id = arr[1];
+    while (wait_min) {
+        sleep(1);
+    }
+    arr[0] = num_of_connected_players;
+    arr[1] = player_id;
+    size_t ns;
+    if (send(newSocket, arr, 2*sizeof(int), 0) != 2*sizeof(int))
+        perror("Failed in sending information\n");
+    char move;
+    while (1){
+        if (recv(newSocket, &move, sizeof(char), 0) == sizeof(char)){    	
+            printf("Received move from %d : %d\%c\n", player_id, move, move);
+            if (move == 'X'){
+                alive[player_id] = 0;
+                num_of_alive_players--;
+                printf("Player %d dead!\n", player_id);
+                break;
+            }
+            network_data[player_id] = move;
+        }
+        else{
+            alive[player_id] = 0;
+            num_of_alive_players--;
+            printf("Player %d got disconnected\n", player_id);
+            break;
+        }
     }
     close(newSocket);
     pthread_exit(NULL);
@@ -83,55 +91,44 @@ void* work_pthread(void * dataptr){
 
 
 int main(){
-
-///////////////////////////////////
     printf("Enter server's IP Address : \n");
     char my_ip_address[40];
-    scanf("%s",my_ip_address);
+    //scanf("%s",my_ip_address);
+    strcpy(my_ip_address, "172.17.49.75");
     int tcp_port_no;
     printf("Enter my TCP port no : \n");
-    scanf("%d",&tcp_port_no);
-    int udp_port_no;
-    printf("Enter my UDP port no : \n");
-    scanf("%d",&udp_port_no);
-
-////////////////////////////////////
-    signal_bind_wrapper(SIGALRM, stop_listening);
-    printf("Listening for players\n");
+    //scanf("%d",&tcp_port_no);
+    tcp_port_no = 8005;
     serverSocket = 0;
     int newSocket = 0;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     int status = bind_wrapper(serverSocket, my_ip_address, tcp_port_no, 1);
-    if (listen( serverSocket, 1)) {
+    if (listen( serverSocket, 3)) {
         perror("Some problem in listen\n");
         exit(2);
     }
     pthread_t tid[MAX_PLAYERS];
     int i;
+    wait_min = 1;
+    signal_bind_wrapper(SIGALRM, stop_listening);
+    printf("Listening for players\n");
     start_timer(waiting_time, 0);
     for (i = 0; i < MAX_PLAYERS && wait_min; i++){
         newSocket = accept(serverSocket,NULL,NULL);
+        socket_data[num_of_connected_players] = newSocket;
+        alive[num_of_connected_players] = 1;
         if (newSocket != -1){
             int *arr = (int*)malloc(sizeof(int)*2);
             arr[0] = newSocket; arr[1] = num_of_connected_players++;
-            pthread_create(&tid[i], NULL, work_pthread,arr);
+            pthread_create(&tid[i], NULL, client_handler,arr);
         }
     }
+    printf("Number of connected players : %d\n", num_of_connected_players);
+    pthread_t sender;
+    pthread_create(&sender, NULL, work_sender, NULL);
     for (i = 0; i < num_of_connected_players; i++)
         pthread_join(tid[i], NULL);
-    close(serverSocket);
-
-//////////////////////////////////////
-    printf("Starting game\n");
-    printf("Number of players connected : %d\n", num_of_connected_players);
-    num_of_alive_players = num_of_connected_players;
-    for (i=0;i<num_of_alive_players;i++) alive[i] = 1;
-    serverSocket = socket(AF_INET, SOCK_DGRAM,0);
-    bind_wrapper(serverSocket, my_ip_address, udp_port_no,0);
-    while (1){
-        receive_moves();
-        send_moves();
-    }
+    pthread_join(sender, NULL);
     printf("Game over!\n");
     close(serverSocket);
     return 0;
